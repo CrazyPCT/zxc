2c746fe20447e5978b535b852181f3f2e027266c
CrazyPCT <125193134+CrazyPCT@users.noreply.github.com>
2023-02-12 20:13:10 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -51,10 +51,10 @@ local function Import_Files()
     makefolder("xGamer626Parkour/Source")
 
     -- Make the files.
-    writefile("xGamer626Parkour/Modules/Util.lua", game:HttpGetAsync("https://raw.githubusercontent.com/xgamerman626/Hub/main/Modules/Util.lua"))
-    writefile("xGamer626Parkour/Modules/UI.lua", game:HttpGetAsync("https://raw.githubusercontent.com/xgamerman626/Hub/main/Modules/UI.lua"))
-    writefile("xGamer626Parkour/Modules/ACB.lua", game:HttpGetAsync("https://raw.githubusercontent.com/xgamerman626/Hub/main/Modules/ACB.lua"))
-    writefile("xGamer626Parkour/Source/Runtime.lua", game:HttpGetAsync("https://raw.githubusercontent.com/xgamerman626/Hub/main/Source/Runtime.lua"))
+    writefile("xGamer626Parkour/Modules/Util.lua", game:HttpGetAsync("https://raw.githubusercontent.com/CrazyPCT/zxc/main/Util.lua"))
+    writefile("xGamer626Parkour/Modules/UI.lua", game:HttpGetAsync("https://raw.githubusercontent.com/CrazyPCT/zxc/main/UI.lua"))
+    writefile("xGamer626Parkour/Modules/ACB.lua", game:HttpGetAsync("https://raw.githubusercontent.com/CrazyPCT/zxc/main/ACB.lua"))
+    writefile("xGamer626Parkour/Source/Runtime.lua", game:HttpGetAsync("https://raw.githubusercontent.com/CrazyPCT/zxc/main/Runtime.lua"))
 end
 
 local function Init()
